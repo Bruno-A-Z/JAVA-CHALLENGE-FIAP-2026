c7c48f8a69d5da82291d43d27f11e32d04de5827
Bruno-A-Z <zanateli@hotmail.com>
2026-05-09 21:20:45 -0300

    Initial commit

---
+++ challengeClyvo/src/main/java/br/com/fiap/challengeClyvo/ChallengeClyvoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challengeClyvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeClyvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeClyvoApplication.class, args);
+	}
+
+}
---
+++ challengeClyvo/src/test/java/br/com/fiap/challengeClyvo/ChallengeClyvoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challengeClyvo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeClyvoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
