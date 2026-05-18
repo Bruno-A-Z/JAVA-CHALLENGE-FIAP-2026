0dc717d8831008ed25b8508c07f2bf9151bece9b
Bruno-A-Z <zanateli@hotmail.com>
2026-05-18 14:42:42 -0300

    FIX: Arrumando metodo

--- challengeClyvo/src/main/java/br/com/fiap/challengeClyvo/repository/TutorRepository.java
+++ challengeClyvo/src/main/java/br/com/fiap/challengeClyvo/repository/TutorRepository.java
@@ -4,12 +4,13 @@ import br.com.fiap.challengeClyvo.model.Tutor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface TutorRepository extends JpaRepository<Tutor, Long> {
 
 
-    List<Tutor> findByCpf(String cpf);
+    Optional<Tutor> findByCpf(String cpf);
     List<Tutor> findByNome(String nome);
 
 }
