fe8d1a9e49a8876b7ea36a6a95d46ec7678eef64
Bruno-A-Z <zanateli@hotmail.com>
2026-05-21 22:50:25 -0300

    FIX/FEAT: Foi adicionado PAGINACAO(pageable) nos metodos

--- challengeClyvo/src/main/java/br/com/fiap/challengeClyvo/repository/AgendamentoRepository.java
+++ challengeClyvo/src/main/java/br/com/fiap/challengeClyvo/repository/AgendamentoRepository.java
@@ -2,18 +2,21 @@ package br.com.fiap.challengeClyvo.repository;
 
 import br.com.fiap.challengeClyvo.enums.StatusAgendamento;
 import br.com.fiap.challengeClyvo.model.Agendamento;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-
+@Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
 
 
-    List<Agendamento> findByStatus(StatusAgendamento status);
-    List<Agendamento> findByVeterinarioId(Long idVet);
-    List<Agendamento> findByPetId(Long idPet);
+    Page<Agendamento> findByStatus(StatusAgendamento status, Pageable pageable);
+    Page<Agendamento> findByVeterinarioId(Long idVet, Pageable pageable);
+    Page<Agendamento> findByPetId(Long idPet, Pageable pageable);
     List<Agendamento> findByDataHoraBetween(LocalDateTime inicio, LocalDateTime fim);
     List<Agendamento> findByVeterinarioIdAndDataHora(long idVet, LocalDateTime dataHora);
 }
