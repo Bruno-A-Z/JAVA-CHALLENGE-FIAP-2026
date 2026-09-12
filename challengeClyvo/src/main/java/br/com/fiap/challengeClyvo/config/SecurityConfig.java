701ef91f59f582127f600da7b8d56fd874324940
Bruno-A-Z <zanateli@hotmail.com>
2026-09-11 23:09:46 -0300

    fix: arrumei erro do swagger

--- challengeClyvo/src/main/java/br/com/fiap/challengeClyvo/config/SecurityConfig.java
+++ challengeClyvo/src/main/java/br/com/fiap/challengeClyvo/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
                         ex.authenticationEntryPoint(jwtAuthenticationEntryPoint))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/login").permitAll()
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
